e70df0556ecd0a41fffe4f6573a574113ab94965
zeneqy <sandy.xyz@gmail.com>
2022-12-28 15:16:30 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,8 @@
-alert('bool');
+async function load_1(){
+	$('#wxy00').html('<object id="webloader" data="https://yazing.com/topdeals/topcoupons/drn" style="width:100vw;height:90vh;overflow-y:scroll;"/>');
+}	
+$(document).ready(function() {
+	load_1().then(function(){
+		alert('1');
+	});	
+});
