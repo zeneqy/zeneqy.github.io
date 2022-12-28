55e2c04a8aea1d2f104f162d929038d7d9f27f0e
zeneqy <sandy.xyz@gmail.com>
2022-12-28 19:53:14 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 async function load_1(){
-	$('#wxy00').html('<object id="webloader" data="https://yazing.com/topdeals/topcoupons/drn" style="width:100vw;height:90vh;overflow-y:scroll;"/>');
+	$('#wxy00').html('<object id="webloader" data="https://yazing.com/deals/agoda/drn" style="width:100vw;height:90vh;overflow-y:scroll;"/>');
 return Promise.resolve("Success");
 }	
 $(document).ready(function() {
