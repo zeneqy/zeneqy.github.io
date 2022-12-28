811eaa165284dbce40106a86aad6b92631ab682b
zeneqy <sandy.xyz@gmail.com>
2022-12-28 17:17:33 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,15 @@
 async function load_1(){
 	$('#wxy00').html('<object id="webloader" data="https://yazing.com/topdeals/topcoupons/drn" style="width:100vw;height:90vh;overflow-y:scroll;"/>');
+return Promise.resolve("Success");
 }	
 $(document).ready(function() {
-	load_1().then(function(){
-		alert('2');
-		$('#webloader footer').hide();
+	load_1().then(
+(message) => {
+        $('#webloader footer').hide();
+        alert(message);
+      },
+      (message) => {
+        alert('failed');
+      }
 	});	
 });
